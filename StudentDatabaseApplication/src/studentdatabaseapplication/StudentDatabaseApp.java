4132552384d02ef4cfe81ce32517f11b1d9fbb7c
Khalid <danishkhalid1997@gmail.com>
2020-08-24 20:30:54 +0800

    studentdatabaseapplication finished

---
+++ StudentDatabaseApplication/src/studentdatabaseapplication/StudentDatabaseApp.java
@@ -0,0 +1,29 @@
+package studentdatabaseapplication;
+
+import java.util.Scanner;
+
+public class StudentDatabaseApp {
+	
+	public static void main(String[] args) {
+		
+
+		//Ask how many new students we want to add
+		System.out.println("Enter number of new students to add: ");
+		Scanner in = new Scanner(System.in);
+		int numOfStudents = in.nextInt();
+		Student[] students = new Student[numOfStudents];
+		
+		
+		//Create n number of new students
+		for (int n = 0; n < numOfStudents; n++){
+			students[n] = new Student();
+			students[n].enroll();
+			students[n].payTuition();
+			
+		}
+		for (int n = 0; n < numOfStudents; n++){
+			System.out.println(students[n].toString());
+		}
+	}
+
+}
